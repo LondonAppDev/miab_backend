865c4c26559117535cb7a0b0be0dafe1f6ef5fd0
Mark Winterbottom <mark@winterbottom.me>
2016-03-29 07:34:55 +0100

    Configured the database, added the model and created the migration file.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
+# Import the 'models' package.
 from django.db import models
 
-# Create your models here.
+
+# Create the class to describe the model.
+class Message(models.Model):
+    """Stores messages for our bottles."""
+
+    # Create a text column to store the message. Limit this to 120
+    # characters as we this isn't "Essey in a Bottle" :)
+    message = models.CharField(max_length=120)
+    # Add a boolean column for marking messages as read, so we can
+    # filter them to ensure users only get new messages.
+    is_read = models.BooleanField(default=False)
+
+    # This is meta data about the database table.
+    class Meta:
+        # Set the table name.
+        db_table = 'message'
--- miab_backend/settings.py
+++ miab_backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,11 +77,12 @@ WSGI_APPLICATION = 'miab_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'miab_db',
+        'USER': 'root',
+        'PASSWORD': 'password',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
